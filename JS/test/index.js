4a104fd6de243843a7cc24c17703b48e3e478578
hayato <hayato.nct127@gmail.com>
2026-05-19 22:48:33 +0900

    add html and js

---
+++ JS/test/index.js
@@ -0,0 +1,46 @@
+// 変数
+let example = 'Hello World!';
+// example = 'Hello World2!';
+
+// 定数
+const example2 = 'He..Hell..Hello World!';
+// example2 = 'Hello World2!';
+
+// 配列
+let inoki = ['いーち', 'にー', 'さん', 'だー！'];
+
+// ループ文
+// let i = 0;
+// while(i < inoki.length){
+//     i++;
+//     console.log(inoki[i]);
+// }
+
+// if/else
+// if(inoki.length >5) {
+//     console.log('ボンバイエ！');
+// } else {
+//     console.log('盆場');
+// }
+
+//関数
+const test = (arg) => {
+    if(inoki.length >arg) {
+        console.log('ボンバイエ！');
+    } else {
+        console.log('盆場');
+    }
+};
+
+//オブジェクト
+const obj = {
+    color: 'red',
+    size: 'big',
+    shape: 'circle',
+    goToilet: () => {
+        console.log('トイレに行きます');
+    }
+}
+window.alert('momomo');
+
+
